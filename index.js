9a4d12849bd64cd51edb222f198983dd90b49611
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2023-09-29 10:37:02 +0200

    test rear cam on phone

--- index.js
+++ index.js
@@ -64,7 +64,9 @@ function startup() {
   startbutton = document.getElementById("startbutton");
 
   navigator.mediaDevices
-    .getUserMedia({ video: true, audio: false })
+    .getUserMedia({ video: {
+      facingMode: "environment"
+    }, audio: false })
     .then((stream) => {
       video.srcObject = stream;
       video.play();
