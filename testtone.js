657a27b85a6bc6a6b1f104e5352c7d625b3c8c08
Djibril Ramdani <djibril.ramdani@etu.univ-amu.fr>
2023-11-07 17:27:07 +0100

    Rajout de la notion de téléchargement de son

--- testtone.js
+++ testtone.js
@@ -1,12 +1,16 @@
 // Création d'un synthétiseur
 const synth = new Tone.Synth().toDestination();
 
+// Création d'une destination de flux de médias à partir du contexte audio de Tone.js
+const audioContext = Tone.getContext();
+const dest = audioContext.createMediaStreamDestination();
+synth.connect(dest); // Connectez le synthétiseur à la destination de flux
+
 // Fonction pour jouer une note de piano
 function playPianoNote() {
-    // Ici, nous jouons la note C4 pendant une durée de '8n'
-    console.log("htfhtrhf");
+    console.log("Note de piano jouée");
     synth.triggerAttackRelease('C4', '8n');
 }
 
 // Lier la fonction au clic sur l'élément "PIANO"
-document.querySelector('li[onclick="changeGenre(\'PIANO\')"]').addEventListener('click', playPianoNote);
+document.querySelector('li[onclick="playPianoNote()"]').addEventListener('click', playPianoNote);
