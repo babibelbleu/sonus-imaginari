8c419f39d6ae211d76f86b9f5ff2eb7831a52b62
Djibril Ramdani <djibril.ramdani@etu.univ-amu.fr>
2024-01-15 11:46:55 +0100

    pouvoir enregistrer un partie des frequances

--- color_manager.js
+++ color_manager.js
@@ -8,42 +8,34 @@
  * @returns {number} La longueur d'onde de la couleur
  */
 function rgbToWavelength(r, g, b) {
-  // On normalise les composants RGB entre 0 et 1
-  const red = r / 255;
-  const green = g / 255;
-  const blue = b / 255;
-
-  // On utilise l'intensité de la couleur dominante pour estimer la longueur d'onde
-  if (green + blue < red) {
-      console.log("Rouge");
+    // On normalise les composants RGB entre 0 et 1
+    const red = r / 255;
+    const green = g / 255;
+    const blue = b / 255;
+  
+    // On utilise l'intensité de la couleur dominante pour estimer la longueur d'onde
+    if (green + blue < red) {
       return normalizeValue(green, 0, 1, 520, 565); // Rouge
-  } else if (red + blue < green) {
-      console.log("Vert");
+    } else if (red + blue < green) {
       return normalizeValue(blue, 0, 1, 450, 500); // Vert
-  } else if (red + green < blue) {
-      console.log("Bleu");
+    } else if (red + green < blue) {
       return normalizeValue(red, 0, 1, 625, 740); // Bleu
-  } else if (red > 200 && green > 200 && blue < 100) {
-      console.log("Jaune");
-      return normalizeValue(green, 0, 1, 570, 590); // Jaune
-  } else if (red > 200 && green < 100 && blue > 200) {
-      console.log("Magenta");
-      return normalizeValue(red, 0, 1, 380, 450); // Magenta
-  } else if (red > 200 && green < 100 && blue < 100) {
-      console.log("Orange");
-      return normalizeValue(red, 0, 1, 590, 620); // Orange
-  } else if (red < 100 && green < 100 && blue > 200) {
-      console.log("Violet");
-      return normalizeValue(blue, 0, 1, 380, 450); // Violet
-  } else if (blue - green < 10 && green - blue < 10 && red - blue > 20 && red - green > 20) {
-      console.log("Cyan");
+    } else if (
+      (red - green < 10 || green - red < 10) &&
+      (red - blue > 20 || green - blue > 20)
+    ) {
+      if (red > 200 && green > 200 && blue < 100) {
+        return normalizeValue(green, 0, 1, 570, 590); // Jaune
+      }
+    } else if (
+      (blue - green < 10 || green - blue < 10) &&
+      (blue - red > 20 || green - red > 20)
+    ) {
       return normalizeValue(blue, 0, 1, 490, 520); // Cyan
-  } else {
-      console.log("Autre couleur (noir, blanc, gris, etc.)");
-      return -1; // Pour les autres couleurs
-  }
+    } else {
+      return -1; // Pour les autres coleurs (noir, blanc, gris et monochromatique)
+    }
 }
-
 
 /**
  * Fonction qui permet d'afficher la catégorie de couleur d'une couleur
