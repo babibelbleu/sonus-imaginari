10b2f010dd039c5b87a2f1e5b91e89f6a5668976
Laniakea17 <sara.rifi2003@gmail.com>
2024-01-16 11:52:02 +0100

    css

--- download.js
+++ download.js
@@ -1,51 +1,57 @@
 /*
  Déclaration de variables pour l'enregistrement audio
-*/
+/
 let mediaRecorder;
 let audioChunks = [];
 let isRecording = false;
 
-/*
+/
  Fonction asynchrone pour basculer l'enregistrement
 */
 async function toggleRecording() {
   const recordButton = document.getElementById('recordButton');
-  
-  if (!isRecording) {
 
-    // Commencez l'enregistrement
-    mediaRecorder = new MediaRecorder(dest.stream);
+  // Vérifie si l'enregistrement a déjà commencé
+  if (!isRecording) {
+    // Obtient le flux audio
+    const stream = await navigator.mediaDevices.getUserMedia({ audio: true, video: false });
+    mediaRecorder = new MediaRecorder(stream);
+
+    // Ajoute les données audio aux morceaux lorsque disponibles
     mediaRecorder.ondataavailable = event => {
       audioChunks.push(event.data);
     };
+
+    // Commence l'enregistrement
     mediaRecorder.start();
     recordButton.textContent = "Terminer l'enregistrement";
     isRecording = true;
   } else {
-
-    // Arrêtez l'enregistrement
+    // Arrête l'enregistrement
     mediaRecorder.stop();
+
+    // À la fin de l'enregistrement, traite et télécharge l'audio
     mediaRecorder.onstop = () => {
-      // Créer un Blob audio à partir des morceaux enregistrés
+      // Crée un Blob audio à partir des morceaux enregistrés
       const audioBlob = new Blob(audioChunks, { type: 'audio/mp3' });
 
-      // Créer une URL pour le Blob audio
+      // Crée une URL pour le Blob audio
       const audioUrl = URL.createObjectURL(audioBlob);
 
-      // Créer un élément de lien pour télécharger le fichier audio
+      // Crée un élément de lien pour télécharger le fichier audio
       const a = document.createElement('a');
-
-      a.style.display = 'none';
       a.href = audioUrl;
       a.download = 'enregistrement.mp3';
+      a.style.display = 'none';
       document.body.appendChild(a);
       a.click();
-      window.URL.revokeObjectURL(audioUrl);
 
-      // Réinitialiser l'interface utilisateur et l'état d'enregistrement
-      recordButton.textContent = "Commencer l'enregistrement";
+      // Nettoie et réinitialise l'état
+      window.URL.revokeObjectURL(audioUrl);
+      document.body.removeChild(a);
       audioChunks = [];
       isRecording = false;
+      recordButton.textContent = "Commencer l'enregistrement";
     };
   }
 }
