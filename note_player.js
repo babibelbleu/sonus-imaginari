59014d7d26335ce3565e924a4108f7235d4cdea5
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2024-01-18 02:40:34 +0100

    Comments

--- note_player.js
+++ note_player.js
@@ -1,35 +1,10 @@
 /**
- * Fonction qui permet de jouer une note en fonction de la longueur d'onde
- * et qui convertit la longueur d'onde en fréquence.
+ * Fonction qui permet de jouer une note à partir d'une fréquence
  * 
- * @param {number} nanometer La longueur d'onde de la couleur en nanomètres
+ * @param {number} frequency La fréquence en Hz
  */
- function playNote(nanometer){
-  
-    /**
-     * Vitesse de la lumière en m/s
-     */
-    // const SPEED_OF_LIGHT = 299792458;
-  
-    // // On convertit la longueur d'onde en mètres
-    // const wavelength = convertToMeter(nanometer);
-  
-    // // On calcule la fréquence de la note
-    // const frequency = SPEED_OF_LIGHT / wavelength;
-  
-    // // On normalise la fréquence pour qu'elle soit dans la plage audible
-    // // car la formule scientifique précédente donne une fréquence en THz
-    // const frequencyNormalized = normalizeValue(frequency, 405000000000000, 700000000000000, 20, 20000);
-  
-    // console.error("FREQUENCY :", frequencyNormalized);
-  
-    // On joue la note pour une durée de 0.1s
-    // synth.triggerAttackRelease(frequencyNormalized, 0.1);
+ function playNote(frequency){
 
-    // on joue la note pour une durée de 0.1s
-    let frequency = nanometer;
-
-    // if NaN, we don't play note
     if (isNaN(frequency)) return;
 
     synth.triggerAttackRelease(frequency, 0.1);
