cf062bd35aa851bd637d2208861b96c73a79ffb5
Bastien Bodin <bastien.bodin.etu@univ-lemans.fr>
2024-01-18 02:59:10 +0100

    Fixed bug not showing at first click

--- FAQ.js
+++ FAQ.js
@@ -1,6 +1,6 @@
 document.querySelectorAll('.faq-question').forEach(item => {
     item.addEventListener('click', () => {
         const answer = item.nextElementSibling;
-        answer.style.display = answer.style.display === 'none' ? 'block' : 'none';
+        answer.style.display = answer.style.display === 'none' || answer.style.display === "" ? 'block' : 'none';
     });
 });
